5e5d292b6299939a42fdcc5a64b6430c9c5c0ab0
zzVIPz <viperexe@mail.ru>
2020-03-04 13:56:27 -0500

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    bracketsConfig = bracketsConfig.join().replace(/,/g, "");
+    for (let i = 0; i < bracketsConfig.length; i += 2) {
+        let tmp = bracketsConfig.slice(i, i + 2);
+        while (~str.indexOf(tmp)) {
+            str = str.replace(tmp, "");
+            i = -2;
+        }
+    }
+    return str ? false : true;
+};
